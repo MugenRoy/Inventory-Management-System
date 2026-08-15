177515b498b0d73b6fa7712861ea47563a076c84
Shaswata Roy <shaswataroy05@gmail.com>
2026-08-15 14:43:19 +0600

    install express

---
+++ script.js
@@ -0,0 +1,12 @@
+function addItem() {
+  const item = {
+      name: document.getElementById('name').value,
+      category: document.getElementById('category').value,
+      quantity: document.getElementById('quantity').value,
+      price: document.getElementById('price').value
+  };
+  console.log(item);
+
+
+
+}
